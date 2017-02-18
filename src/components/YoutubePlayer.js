d371bcbebb50d5893b6e473a682c1ae1d1a3831c
ZaL <z4ls@live.com>
2017-02-18 16:29:02 +0300

    hide youtube player on load

--- src/components/YoutubePlayer.js
+++ src/components/YoutubePlayer.js
@@ -2,7 +2,7 @@
  * @flow
  */
 import React, { PropTypes, Component } from "react";
-import { View, StyleSheet, TouchableHighlight } from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import Youtube from 'react-native-youtube';
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
@@ -12,21 +12,47 @@ export default class YoutubePlayer extends Component {
     style: View.propTypes.style
   };
 
+  state = {
+    isPlaying : false
+  };
+
+  onThumbnailPress() {
+    return this.setState({
+      isPlaying : true
+    });
+  }
+
   render() {
     const { video, style, } = this.props;
+    const { isPlaying } = this.state;
+    const imageThumbnail = `https://img.youtube.com/vi/${video}/hqdefault.jpg`;
     return (
       <View style={[styles.container, style]}>
-        <Youtube
-          ref="youtubePlayer"
-          videoId={video} // The YouTube video ID
-          play={false}           // control playback of video with true/false
-          hidden={true}        // control visiblity of the entire view
-          playsInline={false}    // control whether the video should play inline
-          loop={false}          // control whether the video should loop when ended
-          style={{flex:1, height: 300, width:300,backgroundColor: 'black', marginVertical: 10}}
-          modestbranding={true}
-          showInfo={false}
-        />
+
+        {
+          isPlaying ?
+            <Youtube
+              ref="youtubePlayer"
+              videoId={video} // The YouTube video ID
+              play={isPlaying}           // control playback of video with true/false
+              hidden={isPlaying}        // control visiblity of the entire view
+              playsInline={false}    // control whether the video should play inline
+              loop={false}          // control whether the video should loop when ended
+              style={{flex:1, height: 300, width:300,backgroundColor: 'black', marginVertical: 10}}
+              rel={0}
+              modestbrand={0}
+              showinfo={0}
+            />
+            :
+            <TouchableHighlight
+              onPress={()=>this.onThumbnailPress()}
+              underlayColor="transparent"
+            >
+              <Image source={{uri:imageThumbnail}} style={styles.thumbnailImage} resizeMode="contain"/>
+            </TouchableHighlight>
+
+        }
+
       </View>
     );
   }
@@ -35,17 +61,24 @@ export default class YoutubePlayer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 10
+    paddingTop: 10,
+    backgroundColor:'white',
+    alignItems:'center'
   },
   videoPlayer: {
     width: 225,
     height: 225,
-    alignSelf: "center"
+    alignSelf: "center",
+    backgroundColor:'white',
   },
   removeButton: {
     position: "absolute",
     zIndex: 1000,
     backgroundColor: "transparent",
     marginLeft: 55
+  },
+  thumbnailImage:{
+    width:250,
+    height:250
   }
 });
